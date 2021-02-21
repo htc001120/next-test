a9b572b21e39c4620f7572b61f143cc388f4021a
Ming Chan <stupidming@gmail.com>
2021-02-21 18:55:31 +0000

    Minor styling

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Typography from '@material-ui/core/Typography';
 import SearchResultList from './SearchResultList';
 
-const Result =  () => {
+const Result = () => {
   const items: any = useSelector((state: any) => state.search.items);
   return (
     <>
       { items.length > 0 && <SearchResultList />}
-      { (!items || items.length === 0) && <div>No Result</div>}
+      { (!items || items.length === 0) && <Typography variant="h5">No Result</Typography>}
     </>
   )
 }

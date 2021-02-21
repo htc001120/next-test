d99424939b8eb47399cb186666d3bfac69548bbb
Ming Chan <stupidming@gmail.com>
2021-02-21 19:14:51 +0000

    Basic testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from './reducers';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  applyMiddleware(sagaMiddleware)
+);
+
+test('Render SearchField', () => {
+  render(<Provider store={store}><App /></Provider>);
+  const searchField = screen.getByText(/Keyword/i);
+  expect(searchField).toBeInTheDocument();
+});
+
+test('Render No Result field', () => {
+  render(<Provider store={store}><App /></Provider>);
+  const list = screen.getByText(/No Result/i);
+  expect(list).toBeInTheDocument();
 });
